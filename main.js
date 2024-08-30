3d03660c1d2816a853dbb50a9a47384ca2f11f24
Murilo Vicente <m.vicentemachado2211@gmail.com>
2024-08-29 21:52:19 -0300

    Main page adicionada!

---
+++ main.js
@@ -0,0 +1,13 @@
+let menuItem = document.querySelectorAll('.item-menu')
+
+function selectLink(){
+    menuItem.forEach((item)=>
+        item.classList.remove('ativo')
+    )
+    this.classList.add('ativo')
+}
+
+
+menuItem.forEach((item)=>
+    item.addEventListener('click', selectLink)
+)
